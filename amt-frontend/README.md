ab7affbe451dc8fac5b6f1cd803e34c107893eb6
Mansi <mansisharma3120@gmail.com>
2023-09-09 19:00:52 +0530

    project committed

---
+++ amt-frontend/README.md
@@ -0,0 +1,5 @@
+# AMT- Frontend
+- AMT is an attendance management web application that can track your attendance percentage according to the amount of classes you have attended <br>
+- Tech Stack: React, CSS, Node, Express, MongoDB <br>
+- Visit Site: https://attendify-aashxx.vercel.app/
+"# amt-frontend" 
---
+++ amt-frontend/src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../index.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer style={{padding:'10px'}}>
+            <div className="footer-items">
+            <div className="f-items">
+                    <h1>&lt; AMT &gt;</h1>
+                    <p style={{marginTop: '30px', fontSize: '1.2rem', opacity: '0.5'}}>© Copyright 2023. All Rights Reserved by AMT</p>
+                </div>
+                <div className="f-items" style={{fontSize:'1.6rem'}}>Our Team
+                    <ul>
+                        <li style={{fontSize:'1.2rem'}}><i className="fa-solid fa-user"></i> Mansi</li>
+                        <li style={{fontSize:'1.2rem'}}><i className="fa-solid fa-user"></i> Tanishq</li>
+                    </ul>
+                </div>
+                <div className="f-items" style={{fontSize:'1.6rem'}}>Connect Us
+                    <ul>
+                        <li style={{fontSize:'1.2rem'}}><a href="www.linkedin.com/in/mansi-sharma-aaa4b8229" target="_blank">Mansi's Linkedin</a></li>
+                        <li style={{fontSize:'1.2rem'}}><a href="www.linkedin.com/in/tanishq-sadija-64b1bb24a" target="_blank">Tanishq's Linkedin</a></li>
+                        <li style={{fontSize:'1.2rem'}}><a href="github.com/mansisharma31" target="_blank">Github</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer;
